417f872e1534960bda8204b726d6281be1cafb31
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-02-16 17:33:35 +0100

    ~Security Patch

--- src/main/java/us/elite/scholar/ScholarApi.java
+++ src/main/java/us/elite/scholar/ScholarApi.java
@@ -2,7 +2,7 @@ package us.elite.scholar;
 
 public enum ScholarApi {
 
-    KEY("5e6e7d4b1607cdcf63758da7024b75b5abbf2db3b7cfacef426710753071adb2"),
+    KEY("KEY_HERE"),
     PROFILES("https://serpapi.com/search?engine=google_scholar_profiles"),
     AUTHORS("https://serpapi.com/search?engine=google_scholar_author");
 
